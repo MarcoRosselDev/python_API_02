2dcaba811a63a441e6f55f98ffffd55b1957bd53
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-08 22:37:16 -0300

    api 3

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    title: str
+    content: str
 
 app = FastAPI()
 
@@ -12,3 +17,8 @@ def read_root():
 @app.get("/posts")
 def get_posts():
     return {"data":"There will be posts comingsoon"}
+
+@app.post("/posting")
+def posting(body:dict = Body(...)):
+    print(body)
+    return {'post':body}
