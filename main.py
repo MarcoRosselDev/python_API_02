43bc3575078cd83ee2569e594fbed7b5658fb801
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-10 03:53:19 -0300

    pydantic schema

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from typing import Union
-from fastapi import FastAPI, Body
-from pydantic import BaseModel
-
-class Item(BaseModel):
-    title: str
-    content: str
+from fastapi import FastAPI
+from fastapi.params import Body
+from schema import Post
 
 app = FastAPI()
 
@@ -19,6 +16,19 @@ def get_posts():
     return {"data":"There will be posts comingsoon"}
 
 @app.post("/posting")
-def posting(body:dict = Body(...)):
-    print(body)
-    return {'post':body}
+def posting(
+    #body:dict = Body(...)): ---> Body extrae el cuerpo del post
+    # from fastapi.params import Body ---> nesecita import Body para funcionar
+    # es mejor usar pydantic como libreria aparte para extraer y esquematizar los datos requeridos
+    new_post:Post
+    ):
+    print(new_post)
+    return {
+        # we can format this return
+        #'another_think': 'were return',
+        #'title':body['title'],
+        #'content':body['content'] 
+        'another_think': 'were return',
+        'title':new_post.title,
+        'content':new_post.content
+        }
---
+++ schema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+class Post(BaseModel):
+    title: str
+    content: str
+    publicado: bool = True
+
+
+
+""" class Post(BaseModel):  #---> Modelo imortado de pydantic que valida el formato resivido segun un modelo
+    title: str      #----> Valor requerido
+    content: str    #----> Valor requerido. Si no esta, lanza error.
+    published: bool = True  #----> Valor por defecto.
+    reting_optional: Optional[int] = None  #---> Valor opcional """
