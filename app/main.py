661446d01530c091b8e8eda559ea23d6e30eb542
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-10 22:39:32 -0300

    conection successful with postgres

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI, status, HTTPException
 from .schema import Post
+# pip install python-decouple python-dotenv
+from decouple import config # to pass env passwords from .env file
+PASS_DB = config('PASS_DB')
+import psycopg2
 
 # uvicorn main:app --reload ----------> arrancar un servidor local
 # uvicorn app.main:app --reload ------> dentro de una carpeta 
 app = FastAPI()
+
+
+try:
+    # Connect to an existing database
+    #conn = psycopg2.connect(host, database, user, password)
+    conn = psycopg2.connect(host='localhost', database='postgres', user='postgres', password=PASS_DB)
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+    if conn:
+        print('successful conection')
+except:
+    print('no se pudo conectar a la base de datos de postgres')
 
 @app.post("/posts", 
         status_code=status.HTTP_201_CREATED # por ahora, por que regresava 200 = ok
